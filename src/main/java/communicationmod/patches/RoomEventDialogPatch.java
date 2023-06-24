9a6b400641034ff3f266fcfe156618f2827501fb
Colin <me@colinking.co>
2023-06-23 21:51:13 -0400

    serialize event selection

--- src/main/java/communicationmod/patches/RoomEventDialogPatch.java
+++ src/main/java/communicationmod/patches/RoomEventDialogPatch.java
@@ -3,11 +3,14 @@ package communicationmod.patches;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.events.RoomEventDialog;
+import communicationmod.CommunicationMod;
+import communicationmod.GameStateConverter;
 import communicationmod.GameStateListener;
 import javassist.CannotCompileException;
 import javassist.CtBehavior;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @SpirePatch(
         clz= RoomEventDialog.class,
@@ -17,10 +20,15 @@ import java.util.ArrayList;
 public class RoomEventDialogPatch {
 
     @SpireInsertPatch(
-            locator=Locator.class
+            locator=Locator.class,
+            localvars={"i"}
     )
-    public static void Insert(RoomEventDialog _instance) {
+    public static void Insert(RoomEventDialog _instance, int i) {
         GameStateListener.registerStateChange();
+
+        HashMap<String, Object> action = new HashMap<>();
+        action.put("index", i);
+        CommunicationMod.reportAction("SelectOption", action);
     }
 
     private static class Locator extends SpireInsertLocator {
